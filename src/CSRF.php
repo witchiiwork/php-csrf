7f7bf32027820588b9c2ceb8f7f8c8dc245c5dba
Me Proficiency <58886077+mexproficiency@users.noreply.github.com>
2019-12-15 10:19:10 +0700

    Initial commit

--- src/CSRF.php
+++ src/CSRF.php
@@ -2,59 +2,56 @@
 namespace W2W\CSRF;
 
 class CSRF {
-	public const conTOKNAMTXT = "_CSRF_mCzwh300xSkbiO2A1HS27VaDmBHRC5_FRSC_";
+	private $strNAMESPACE;
 	
-	public static function funGETTOKNAM(string $strTOKNAMTXT = self::conTOKNAMTXT): string {
-		return $strTOKNAMTXT;
-	}
-	
-	public static function funVLDTOKDAT(array $arrREQTOKDAT = [], string $strTOKNAMTXT = self::conTOKNAMTXT): bool {
-		if(empty($_SESSION[$strTOKNAMTXT])) {
-			static::funGENTOKXXX($strTOKNAMTXT);
-			
-			return false;
+	public function __construct($strNAMESPACE = "_csrf") {
+		$this->namespace = $strNAMESPACE;
+		
+		if(session_id() === "") {
+			session_start();
 		}
 		
-		if(empty($arrREQTOKDAT[$strTOKNAMTXT])) {
-			return false;
-		}
-		
-		if(static::funCPRTOKTXT($arrREQTOKDAT[$strTOKNAMTXT], static::funGETTOKTXT($strTOKNAMTXT))) {
-			static::funGENTOKXXX($strTOKNAMTXT);
-			
-			return true;
-		}
-
-		return false;
+		$this->setToken();
 	}
 	
-	public static function funGENTOKXXX(string $strTOKNAMTXT = self::conTOKNAMTXT): void {
-		$strSLTRIPUNQ = !empty($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : uniqid();
-		$_SESSION[$strTOKNAMTXT] = sha1(uniqid(sha1($strSLTRIPUNQ), true));
+	public function getToken() {
+		return $this->readTokenFromStorage();
 	}
 	
-	public static function funCPRTOKTXT(string $numINTCNTHST = "", string $numINTCNTNDE = ""): bool {
-		$numINTCNTHNE = strlen($numINTCNTHST) ^ strlen($numINTCNTNDE);
-		$numMINSTRLEN = min(strlen($numINTCNTHST), strlen($numINTCNTNDE));
-		$numINTCNTHST = substr($numINTCNTHST, 0, $numMINSTRLEN);
-		$numINTCNTNDE = substr($numINTCNTNDE, 0, $numMINSTRLEN);
+	public function isTokenValid($strUSERTOKEN) {
+		return ($strUSERTOKEN === $this->readTokenFromStorage());
+	}
+	
+	public function insertInputField() {
+		$strCSRFTOKEN = $this->getToken();
 		
-		for($numINTCNTMIN = 0, $numINTCNTMAX = strlen($numINTCNTHST); $numINTCNTMIN < $numINTCNTMAX; $numINTCNTMIN++) {
-			$numINTCNTHNE += !(ord($numINTCNTHST[$numINTCNTMIN]) === ord($numINTCNTNDE[$numINTCNTMIN]));
+		echo "<input type=\"hidden\" name=\"" . $this->namespace . "\" value=\"" . $strCSRFTOKEN . "\">";
+	}
+	
+	public function verifyRequest() {
+		if(!$this->isTokenValid($_POST[$this->namespace])) {
+			die("CSRF validation failed.");
 		}
-		
-		return !$numINTCNTHNE;
 	}
 	
-	public static function funGETTOKTXT(string $strTOKNAMTXT = self::conTOKNAMTXT): string {
-		if(empty($_SESSION[$strTOKNAMTXT])) {
-			static::funGENTOKXXX($strTOKNAMTXT);
+	private function setToken() {
+		$storedToken = $this->readTokenFromStorage();
+		
+		if($storedToken === "") {
+			$strCSRFTOKEN = md5(uniqid(rand(), true));
+			$this->writeTokenToStorage($strCSRFTOKEN);
 		}
-		
-		return $_SESSION[$strTOKNAMTXT];
 	}
 	
-	public static function funGETFRMHIS(string $strTOKNAMTXT = self::conTOKNAMTXT): string {
-		return sprintf("<input type=\"hidden\" name=\"%s\" value=\"%s\">", $strTOKNAMTXT, static::funGETTOKTXT($strTOKNAMTXT));
+	private function readTokenFromStorage() {
+		if(isset($_SESSION[$this->namespace])) {
+			return $_SESSION[$this->namespace];
+		} else {
+			return "";
+		}
+	}
+	
+	private function writeTokenToStorage($strCSRFTOKEN) {
+		$_SESSION[$this->namespace] = $strCSRFTOKEN;
 	}
 }
